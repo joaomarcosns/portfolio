ba65f4554ec4d42f4e4b450d137042eb8d69ec5f
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2024-06-08 14:25:21 -0300

    chore: add new remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "sa-east-1.graphassets.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
