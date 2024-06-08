054c34ba9bc6a673298effa332966ed7eb4c3c23
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2024-06-08 14:26:41 -0300

    feat: add data by api

--- app/_components/pages/home/HeroSection/index.tsx
+++ app/_components/pages/home/HeroSection/index.tsx
@@ -2,20 +2,15 @@ import Image from "next/image";
 import TechBadge from "../../../TechBadge";
 import Button from "../../../Button";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import { TbBrandGithub, TbBrandLinkedin } from "react-icons/tb";
+import { HomePageInfo } from "@/app/_types/page-info";
+import { RichText } from "@/app/_components/RichText";
+import { CMSIcon } from "@/app/_components/CmsIcon";
 
-const CONST_CONTACTS = [
-  {
-    url: "https://github.com/joaomarcosns",
-    icon: <TbBrandGithub />,
-  },
-  {
-    url: "https://www.linkedin.com/in/joaomarcosns/",
-    icon: <TbBrandLinkedin />,
-  },
-];
+type homeSectionProps = {
+  homeInfo: HomePageInfo;
+};
 
-const HeroSection = () => {
+const HeroSection = ({ homeInfo }: homeSectionProps) => {
   return (
     <section className="flex w-full flex-col justify-end bg-hero-image bg-cover bg-center bg-no-repeat py-32 pb-10 sm:pb-32 lg:h-[755px] lg:pb-[110px]">
       <div className="container flex flex-col-reverse items-start justify-between lg:flex-row">
@@ -25,16 +20,13 @@ const HeroSection = () => {
             João Marcos Neves da Silva
           </h2>
 
-          <p className="my-6 text-sm text-gray-400 sm:text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            possimus ratione quasi vero labore, ut magnam quis quibusdam totam
-            expedita libero neque sapiente quas ipsa minus consequatur dicta
-            recusandae pariatur.
-          </p>
+          <div className="my-6 text-sm text-gray-400 sm:text-base">
+            <RichText content={homeInfo.introduction.raw} />
+          </div>
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
-            {Array.from({ length: 7 }).map((_, index) => (
-              <TechBadge name="NEXT JS" key={index} />
+            {homeInfo.technologies.map((tech, index) => (
+              <TechBadge name={tech.name} key={index} />
             ))}
           </div>
 
@@ -45,14 +37,14 @@ const HeroSection = () => {
             </Button>
 
             <div className="flex h-20 items-center gap-3 text-2xl text-gray-600">
-              {CONST_CONTACTS.map((contact, index) => (
+              {homeInfo.socialMedias.map((contact, index) => (
                 <a
                   href={contact.url}
                   key={`contact-${index}`}
                   target="_blank"
                   className="transition-colors hover:text-gray-100"
                 >
-                  {contact.icon}
+                  <CMSIcon icon={contact.iconSvg} />
                 </a>
               ))}
             </div>
@@ -62,7 +54,7 @@ const HeroSection = () => {
         <Image
           width={420}
           height={404}
-          src="/images/profile.jpg"
+          src={homeInfo.profilePicture.url}
           alt="Foto de perfil de João Marcos Neves da Silva"
           className="mb-6 h-[300px] w-[300px] rounded-lg object-cover shadow-2xl lg:mb-0 lg:h-[404px] lg:w-[420px]"
         />
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,50 @@ import HeroSection from "./_components/pages/home/HeroSection";
 import HighlightedProjects from "./_components/pages/home/HighlightedProjects";
 import KnownTechs from "./_components/pages/home/KnownTechs";
 import WorkExperience from "./_components/pages/home/WorkExperience";
+import { HomePageData } from "./_types/page-info";
+import { fetchHygraphQuery } from "./_utils/fetch-hygraph-query";
 
-const Home = () => {
+export const metadata = {
+  title: "Home",
+};
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+    query PageInfoQuery {
+      home(where: { slug: "home" }) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socialMedias {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconsSvg
+          name
+          startDate
+        }
+      }
+    }
+  `;
+
+  return fetchHygraphQuery(
+    query,
+    1000 * 60 * 60 * 24, // 1 day
+  );
+};
+
+const Home = async () => {
+  const { home: data } = await getPageData();
   return (
     <>
-      <HeroSection />
+      <HeroSection homeInfo={data} />
       <KnownTechs />
       <HighlightedProjects />
       <WorkExperience />
